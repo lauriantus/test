00ea09ee383dbf3539a72a0819b93439383a289b
Ivan <lauriantus@gmail.com>
2022-07-03 10:57:34 +0300

    completed start position

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,9 +1,37 @@
 package com.practice;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-	// write your code here
+    Object[] arr = new Object[4];
+    String[] arrStr = new String[3];
+    arrStr[0] = "str";
+
+        List<String> list = new ArrayList<>();
+        list.add("str");
+        list.add("str1");
+        list.add("str2");
+        list.add("str3");
+        list.add("str4");
+        list.add("str5");
+        list.add("str6");
+        list.add("str7");
+        list.add("str8");
+        System.out.println("List: " + list);
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println("Set: " + set);
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1,"str");
+        map.put(2,"str");
+        map.put(3,"str");
+        map.put(3,"str");
+        System.out.println("Map: " + map);
     }
 }
--- src/com/practice/Test3.java
+++
@@ -1,7 +0,0 @@
-package com.practice;
-
-public class Test3 {
-    public static void main(String[] args) {
-        fassadf
-    }
-}
